755915b5078734af25b20bbf714906f4dc24f755
Ivan Merkulov <zZEXz@yandex.ru>
2016-10-26 16:20:52 +0300

    FirstAndSecondTask

---
+++ task3/task3/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Employee
+    {
+        public string Name;
+        public string Surname;
+        public string Appointment; //начальник отдела. инженер.рабочий
+        public double WorkExperience;
+        public double oclad;
+        public double Nalog;
+        //методы
+        public void CalcOcladAndNalog()
+        {
+           // double oclad=0;
+            if (Appointment=="начальник отдела")
+            {
+                this.oclad = 10000;
+              
+            }
+            if (Appointment == "инженер")
+            {
+                this.oclad = 8000;
+            }
+            if (Appointment == "рабочий")
+            {
+                this.oclad = 4000;
+            }
+             if (WorkExperience>5)
+             { oclad = this.oclad * 1.3; }
+             
+            Nalog = this.oclad*0.13;
+             
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Имя: "+Name);
+            Console.WriteLine("Фамилия: "+Surname);
+            Console.WriteLine("Должность: "+Appointment);
+            Console.WriteLine("Оклад:"+ oclad);
+            Console.WriteLine("НДФЛ="+ Nalog);
+        }
+        //конструктор
+        public Employee(string name1, string surname1)
+        {
+            this.Name = name1;
+            this.Surname = surname1;
+        }
+    }
+}
---
+++ task3/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Employee first = new Employee("Иван", "Владимирович");
+            first.Appointment = "начальник отдела";
+            first.CalcOcladAndNalog();
+            first.ShowInfo();
+            Console.ReadKey();
+
+        }
+    }
+}
